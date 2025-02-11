c7caf7f62b13eac01f708951f0e4d67ea8d48761
yogeshdnumb <dyogesh.official@gmail.com>
2025-02-11 13:28:55 +0530

    fixed tailwind not working

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
-  root: "./src/pages",
+  root: "./src",
   build: {
     outDir: "../../dist",
     emptyOutDir: true,
